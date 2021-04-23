76d1ea0c98977d8bff3dedd8afbf5416ead8548f
felipeki <felipeki@users.noreply.github.com>
2021-04-23 11:01:45 -0300

    Exercicios .Net Console

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+          
+            while (opcaoUsuario.ToUpper() != "X") {
+          
+               
+               switch (opcaoUsuario.ToUpper())
+               {
+                   case "1":
+                   Console.WriteLine("Informe o nome do aluno:");
+                   Aluno aluno = new Aluno();
+                   aluno.Nome = Console.ReadLine();
+                   Console.WriteLine("Informe a nota do aluno:");
+                  if (decimal.TryParse(Console.ReadLine(),out decimal nota))
+                  {
+                      aluno.Nota = nota;
+                  }
+                  else
+                  {
+                      throw new ArgumentException("Valor da nota deve ser decimal");
+                  }
+                  alunos[indiceAluno] = aluno;
+                  indiceAluno++;
+
+
+                   break;
+                   case "2":
+                   foreach(var a in alunos) 
+                   {
+                       if(!string.IsNullOrEmpty(a.Nome))
+                    Console.WriteLine($"Aluno: {a.Nome} - NOTA {a.Nota}");
+                   }
+                   break;
+                   case "3":
+                   decimal notaTotal = 0;
+                   var nrAlunos = 0;
+                   for (int i=0; i < alunos.Length; i++)
+                   {
+                        if(!string.IsNullOrEmpty(alunos[i].Nome))
+                        {
+                           notaTotal = notaTotal + alunos[i].Nota; 
+                           nrAlunos++;
+                        }
+                   }
+                   var mediaGeral = notaTotal / nrAlunos;
+                   Conceito conceitoGeral;
+
+
+                   if (mediaGeral < 2)
+                   {
+                    conceitoGeral = Conceito.E;
+
+                   }else if (mediaGeral < 4)
+                   
+                   {
+                        conceitoGeral = Conceito.D;
+                        
+                    }
+                    else if (mediaGeral < 6)
+                    {
+                       conceitoGeral = Conceito.C;
+                    } 
+                    else if (mediaGeral < 8)
+                    {
+                            conceitoGeral = Conceito.B;
+                       }
+                       else
+                       {
+                           conceitoGeral = Conceito.A;
+                       }
+                   Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral} ");
+
+                   break;
+                   case "X":
+                   Console.WriteLine("Saindo...");
+                   break;
+                   default:
+                   Console.WriteLine("Opcao Invalida");
+                   break;
+                
+                  
+               }
+               ObterOpcaoUsuario();
+            
+            
+            }
+            
+            string ObterOpcaoUsuario() {
+            Console.WriteLine();
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1 - Inserir Novo Aulo:'");
+            Console.WriteLine("2 - Listar Alunos:");
+            Console.WriteLine("3 - Calcular Média Geral:");
+            Console.WriteLine("X - Para Sair");
+            Console.WriteLine();
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+
+            }
+
+    }
+    }
+}
